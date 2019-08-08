fd94893818bbe01a2c4e68b1db57628a8206aa1d
Mona-S <forward2cm@gmail.com>
2019-08-08 22:48:43 +0000

    Completed Javascript and JSON exercise

--- exercises/javascript-and-json/main.js
+++ exercises/javascript-and-json/main.js
@@ -1,1 +1,29 @@
 /* eslint-disable no-console */
+
+var bookArray = [{
+  isbn: 'one',
+  title: 'The Industries of The Future',
+  author: 'Alec'
+},
+
+{
+  isbn: 'two',
+  title: 'Enlightenment Now',
+  author: 'Steven'
+},
+
+{
+  isbn: 'three',
+  title: 'Sustainable Energy',
+  author: 'David'
+}];
+
+var stringJSON = JSON.stringify(bookArray);
+console.log('JSONString: ', stringJSON);
+console.log('typeof(JSONString): ', typeof (stringJSON));
+
+var studentData = '{ "id": "1" , "name": "Mona" }';
+
+var parseJSON = JSON.parse(studentData);
+console.log('ParsedString: ', parseJSON);
+console.log('typeof(ParsedString): ', typeof (parseJSON));
