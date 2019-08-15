d7a471ea0bcaa6ab9008479802c9cd85b4bf41e9
Mona-S <forward2cm@gmail.com>
2019-08-15 00:35:24 +0000

    Completed promises exercise

--- exercises/promises/index.js
+++ exercises/promises/index.js
@@ -1,2 +1,13 @@
 /* eslint-disable no-unused-vars, no-console */
 const takeAChance = require('./take-a-chance');
+
+const promise1 = takeAChance('mona');
+
+promise1.then(value => {
+  console.log(value);
+
+});
+
+promise1.catch(error => {
+  console.log(error);
+});
