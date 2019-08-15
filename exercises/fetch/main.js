a2cf7315e260e78bf58e782b54215bd08807e28e
Mona-S <forward2cm@gmail.com>
2019-08-15 01:18:42 +0000

    Completed fetch exercise

--- exercises/fetch/main.js
+++ exercises/fetch/main.js
@@ -1,1 +1,9 @@
 /* eslint-disable no-console */
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (myJson) {
+    console.log(myJson);
+  });
